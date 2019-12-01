ddcfae075f536b439b2489a07e06e24415a817e1
Shashank <s_shashank@outlook.com>
2019-12-01 15:16:40 +0530

    show post and update post

--- app/blog/views.py
+++ app/blog/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, redirect, url_for, flash
+from flask import Blueprint, render_template, redirect, url_for, flash, abort, request
 from flask_login import login_required, current_user
 from app.blog.forms import PostForm
 from app.models import Post
@@ -38,4 +38,33 @@ def add():
         flash('Your new post has been added successfully', 'success')
         return redirect(url_for('blog.blog'))
 
-    return render_template('add.html', title='New Post', form=form)
+    return render_template('add.html', title='New Post', form=form, header="Add New Post")
+
+@blog_blueprint.route('/post/<post_id>')
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('post.html', title=post.title, post=post)
+
+@blog_blueprint.route('/post/<post_id>/update', methods=['GET', 'POST'])
+@login_required
+def update(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.users != current_user:
+        abort(403)
+
+    form = PostForm()
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+
+        db.session.add(post)
+        db.session.commit()
+
+        flash('Your post has been updated successfully.', 'success')
+        return redirect(url_for('blog.post', post_id=post.id))
+
+    elif request.method == 'GET':
+        form.title.data = post.title
+        form.content.data = post.content
+
+    return render_template('add.html', title='Update Post', form=form, header="Update Your Post", post=post)
